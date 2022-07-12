068ba91b96c145269c89e251bdf1092bd47ad04c
Augusto Custodio <augusto.custodio@outlook.com>
2022-07-12 15:19:03 -0300

    added button at notion style

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,5 +1,18 @@
-import { Button as MuiButton, ButtonProps } from '@mui/material'
+import { Button as MuiButton } from '@mui/material'
 
-const Button = ({ ...props }: ButtonProps) =><MuiButton fullWidth {...props}/>
+const notion_style = {
+    fontSize: '14px',
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: 'rgb(46 170 220)',
+    borderRadius: '3px',
+    textTransform: 'none',
+    '&:hover': {
+        backgroundColor: 'primary',
+        color: 'white',
+    },
+}
+
+const Button = ({ ...props }) =><MuiButton sx={notion_style} disableElevation {...props}/>
 
 export default Button
